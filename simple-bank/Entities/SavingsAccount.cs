7d69c95a2b019bf9601d6330e156e93940a932e8
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-03-17 17:22:31 -0300

    innitial commit

---
+++ simple-bank/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace simple_bank.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            // Call the base class method and add a rule to remove two more for withdrawal fee
+            Balance -= 2.0;
+        }
+    }
+}
